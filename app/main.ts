4624f5da8e2d0a26ea48ba2382820fefcee0b4cb
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-14 12:46:28 -0400

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -163,9 +163,9 @@ import { months, years } from "./constants";
 
     query.outStatistics = [
       new StatisticDefinition({
-        onStatisticField: "1",
+        onStatisticField: "36",
         outStatisticFieldName: "value",
-        statisticType: "count"
+        statisticType: "first"
       })
     ];
     query.groupByFieldsForStatistics = [ "YEAR + '-' + MonthName" ];
@@ -193,9 +193,9 @@ import { months, years } from "./constants";
     const query = layer.createQuery();
     query.outStatistics = [
       new StatisticDefinition({
-        onStatisticField: "1",
+        onStatisticField: "36",
         outStatisticFieldName: "value",
-        statisticType: "count"
+        statisticType: "first"
       })
     ];
     query.groupByFieldsForStatistics = [ "YEAR + '-' + MonthName" ];
