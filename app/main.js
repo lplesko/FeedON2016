b2305111039de18e5023954b8e051abe11b882ca
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-14 14:37:55 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -112,9 +112,9 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layerView.layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "1",
+                                    onStatisticField: "2",
                                     outStatisticFieldName: "value",
-                                    statisticType: "sum"
+                                    statisticType: "first"
                                 })
                             ];
                             query.groupByFieldsForStatistics = ["Year + '-' + MonthName"];
@@ -149,9 +149,9 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "1",
+                                    onStatisticField: "2",
                                     outStatisticFieldName: "value",
-                                    statisticType: "sum"
+                                    statisticType: "first"
                                 })
                             ];
                             query.groupByFieldsForStatistics = ["Year + '-' + MonthName"];
