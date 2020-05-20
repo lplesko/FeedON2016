f90e7aec53c079bbe93ef3653c893f41a14e2355
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-20 12:43:34 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -227,7 +227,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                     });
                     map = new EsriMap({
                         basemap: "gray-vector",
-                        layers: [layer, districtsLayer]
+                        layers: [districtsLayer]
                     });
                     view = new MapView({
                         map: map,
