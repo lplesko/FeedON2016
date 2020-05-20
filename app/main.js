ff4fa64a52add3f2b9cb9af74d3f25f339cc7f8f
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-20 13:31:27 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -112,9 +112,9 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layerView.layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "1",
+                                    onStatisticField: "Total_visits",
                                     outStatisticFieldName: "value",
-                                    statisticType: "count"
+                                    statisticType: "sum"
                                 })
                             ];
                             query.groupByFieldsForStatistics = ["Year + '-' + MonthName"];
@@ -149,9 +149,9 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "1",
+                                    onStatisticField: "Total_visits",
                                     outStatisticFieldName: "value",
-                                    statisticType: "count"
+                                    statisticType: "sum"
                                 })
                             ];
                             query.groupByFieldsForStatistics = ["Year + '-' + MonthName"];
@@ -211,6 +211,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                         },
                         outFields: ["MonthName", "Year"]
                     });
+                    
                     districtsLayer = new FeatureLayer({
                         title: "districts",
                         portalItem: {
@@ -225,6 +226,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             })
                         })
                     });
+                    
                     map = new EsriMap({
                         basemap: "gray-vector",
                         layers: [layer, districtsLayer]
