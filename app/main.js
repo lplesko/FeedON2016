ba40121e445bfd361c6fededaf754381025ccb43
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-22 09:21:28 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -242,7 +242,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                         layers: [annualLayer]
                     });
                     
-                    mapList = ["annualMap", "mainMap"];
+                    mapList = [mainMap, annualMap];
                     
                     view = new MapView({
                         map: mapList[0],
